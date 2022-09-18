3e0267d004ea550c3f4c46cd954cc38ad8d1156a
dhmChouliaras <dhm.chouliaras@gmail.com>
2022-09-18 18:13:49 +0300

    disabled camera limits

--- MainCamera.gd
+++ MainCamera.gd
@@ -6,8 +6,10 @@ onready var top_left = $Limits/TopLeft
 
 
 func _ready():
-	limit_top = top_left.position.y
-	limit_left = top_left.position.x
-	limit_bottom = bottom_right.position.y
-	limit_right = bottom_right.position.x
+	pass
+#	 disable limits
+#	limit_top = top_left.position.y
+#	limit_left = top_left.position.x
+#	limit_bottom = bottom_right.position.y
+#	limit_right = bottom_right.position.x
 
